f137a5db2bbd4a4e12e54d760e8b84f8130c16bd
Renan Reis <renanreismartins@gmail.com>
2012-02-06 07:56:33 -0200

    implementando repositorio de veiculos

---
+++ src/main/java/br/com/renan/infra/VeiculoDAOHibernate.java
@@ -0,0 +1,22 @@
+package br.com.renan.infra;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import br.com.renan.domain.Garagem;
+import br.com.renan.domain.Veiculo;
+
+public class VeiculoDAOHibernate implements Garagem {
+
+	private Session session;
+	
+	public List<Veiculo> lista() {
+		return null;
+	}
+
+	public void compra(Veiculo veiculo) {
+		session.save(veiculo);
+	}
+	
+}
